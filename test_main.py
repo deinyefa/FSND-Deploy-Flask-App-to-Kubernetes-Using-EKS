5eb157a82ccd7cbd406e425feb2f9fff5e7a4670
deinyefa <df.eporwei@gmail.com>
2019-12-01 19:40:22 -0500

    add dependencies and run tests before building + break a test on purpose

--- test_main.py
+++ test_main.py
@@ -1,8 +1,9 @@
 '''
 Tests for jwt flask app.
 '''
-import os
 import json
+import os
+
 import pytest
 
 import main
@@ -26,6 +27,7 @@ def test_health(client):
     response = client.get('/')
     assert response.status_code == 200
     assert response.json == 'Healthy'
+    assert False
 
 
 def test_auth(client):
